3fd7061ec71550a4ab7af18141f94c5cb7728abc
Krual Patel <krunalpatel32@gmail.com>
2018-10-08 15:44:18 -0400

    added .eslintig

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node" : true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread" : true,
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+           "warn"
+        ],
+        "linebreak-style": [
+            "off"
+        ],
+        "quotes": [
+            "warn",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console":[
+            "warn"
+        ]
+    }
+};
